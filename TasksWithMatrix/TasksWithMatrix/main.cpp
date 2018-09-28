f5827673d5c6307dd33cf3944f8aaee837379f7a
AlexStoyanova <al.stoqnova74@gmail.com>
2018-09-28 16:32:06 +0300

    Commit tasks with transposed matrix and multiply square matrix

--- TasksWithMatrix/TasksWithMatrix/main.cpp
+++ TasksWithMatrix/TasksWithMatrix/main.cpp
@@ -37,7 +37,7 @@ void randMatrix(int matrix[][MAX_SIZE], size_t n, size_t m)
 
 		for (size_t j = 0; j < m; j++)
 		{
-			matrix[i][j] = rand() % 100;
+			matrix[i][j] = rand() % 10;
 		}
 	}
 }
@@ -55,14 +55,40 @@ void transposedMatrix(int matrix[][MAX_SIZE], size_t n, size_t m)
 	printMatrix(matrix_1, m, n);
 }
 
+void multiplySquareMatrix(int A[][MAX_SIZE], int B[][MAX_SIZE], size_t n)
+{
+	int C[MAX_SIZE][MAX_SIZE];
+	for (size_t i = 0; i < n; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			int sum = 0;
+			for (size_t m = 0; m < n; m++)
+			{
+				sum += (A[i][m] * B[m][j]);
+			}
+			C[i][j] = sum;
+		}
+	}
+	printMatrix(C, n, n);
+}
+
 int main()
 {
-	int matrix[MAX_SIZE][MAX_SIZE];
-	unsigned int m, n;
-	cin >> n >> m;
-	randMatrix(matrix, n, m);
-	printMatrix(matrix, n, m);
-	transposedMatrix(matrix, n, m);
-
+	//int matrix[MAX_SIZE][MAX_SIZE];
+	//unsigned int m, n;
+	//cin >> n >> m;
+	//randMatrix(matrix, n, m);
+	//printMatrix(matrix, n, m);
+	//transposedMatrix(matrix, n, m);
+	int A[MAX_SIZE][MAX_SIZE];
+	int B[MAX_SIZE][MAX_SIZE];
+	unsigned int n;
+	cin >> n;
+	randMatrix(A, n, n);
+	printMatrix(A, n, n);
+	randMatrix(B, n, n);
+	printMatrix(B, n, n);
+	multiplySquareMatrix(A, B, n);
 	return 0;
 }
