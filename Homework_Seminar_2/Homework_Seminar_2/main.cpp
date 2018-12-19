cf6388efe4fa9624749021ac9e4400d0c5d275ef
AlexStoyanova <al.stoqnova74@gmail.com>
2018-12-19 20:41:29 +0200

    Add second task for Homework 2

---
+++ Homework_Seminar_2/Homework_Seminar_2/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+unsigned int i = 0;
+unsigned int start = 0;
+
+int positionLetter(char *str, char letter, size_t n)
+{
+	if (start == (strlen(str) + 1))
+	{
+		return -1;
+	}
+	if (str[start] == letter)
+	{
+		i++;
+	}
+	if (i == n)
+	{
+		return start;
+	}
+	start++;
+	return positionLetter(str, letter, n);
+}
+
+int main()
+{
+	char str[MAX_SIZE];
+	char letter;
+	unsigned int n;
+	cin >> str;
+	cin >> letter;
+	do
+	{
+		cin >> n;
+	} while (n < 1 || n > 100);
+	cout << positionLetter(str, letter, n);
+
+
+
+	return 0;
+}
