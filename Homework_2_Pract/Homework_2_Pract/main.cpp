5f60b18bb3e15c08e2dc5309d34da40861890149
AlexStoyanova <al.stoqnova74@gmail.com>
2019-01-08 20:14:13 +0200

    Change task

--- Homework_2_Pract/Homework_2_Pract/main.cpp
+++ Homework_2_Pract/Homework_2_Pract/main.cpp
@@ -1,87 +1,27 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 const int SIZE = 100;
-int counter = 0;
 
 bool isValid(int X, int Y, size_t n)
 {
-	if (X < 0 || Y < 0 || X > (n - 1) || Y > (n - 1))
+	if (X < 0 || Y < 0 || X >(n - 1) || Y >(n - 1))
 	{
 		return false;
 	}
 	return true;
 }
 
-void waterInSquare(int matrix[][SIZE], size_t n, size_t X, size_t Y)
+int waterInSquare(int matrix[][SIZE], size_t n, size_t X, size_t Y, int counter = 0)
 {
-	if ((X != 0 && Y != 0 && X != (n-1) && Y != (n - 1)) && 
-		((matrix[X - 1][Y] == 0) && (matrix[X + 1][Y] == 0) && (matrix[X][Y - 1] == 0) && (matrix[X][Y + 1] == 0)))
+	if ((!isValid(X, Y, n)) || matrix[X][Y] == 0)
 	{
-		counter++;
-		return;
+		return counter - 1;
 	}
-	else if (X == 0 && Y == 0 && (matrix[X + 1][Y] == 0 && matrix[X][Y + 1] == 0))
-	{
-		counter++;
-		return;
-	}
-	else if (X == 0 && Y == (n - 1) && (matrix[X][Y - 1] == 0 && matrix[X + 1][Y] == 0))
-	{
-		counter++;
-		return;
-	}
-	else if (X == (n - 1) && Y == (n - 1) && (matrix[X - 1][Y] == 0 && matrix[X][Y - 1] == 0))
-	{
-		counter++;
-		return;
-	}
-	else if (X == (n - 1) && Y == 0 && (matrix[X - 1][Y] == 0 && matrix[X][Y + 1] == 0))
-	{
-		counter++;
-		return;
-	}
-	else if (Y == 0 && matrix[X - 1][Y] == 0 && matrix[X][Y + 1] == 0 && matrix[X + 1][Y] == 0)
-	{
-		counter++;
-		return;
-	}
-	else if (X == (n - 1) && matrix[X][Y - 1] == 0 && matrix[X - 1][Y] == 0 && matrix[X][Y + 1] == 0)
-	{
-		counter++;
-		return;
-	}
-	else if (Y == (n - 1) && matrix[X - 1][Y] == 0 && matrix[X + 1][Y] == 0 && matrix[X][Y - 1] == 0)
-	{
-		counter++;
-		return;
-	}
-	else if (X == 0 && matrix[X][Y - 1] == 0 && matrix[X + 1][Y] == 0 && matrix[X][Y + 1] == 0)
-	{
-		counter++;
-		return;
-	}
-	//matrix[X][Y] == 0;
-	if (isValid(X + 1, Y, n) && matrix[X + 1][Y] == 1)
-	{
-		matrix[X + 1][Y] = 0;
-		waterInSquare(matrix, n, X + 1, Y);
-	}
-	if (isValid(X, Y - 1, n) && matrix[X][Y - 1] == 1)
-	{
-		matrix[X][Y - 1] = 0;
-		waterInSquare(matrix, n, X , Y - 1);
-	}
-	if (isValid(X, Y + 1, n) && matrix[X][Y + 1] == 1)
-	{
-		matrix[X][Y + 1] = 0;
-		waterInSquare(matrix, n, X, Y + 1);
-	}
-	if (isValid(X - 1, Y, n) && matrix[X - 1][Y] == 1)
-	{
-		matrix[X - 1][Y] = 0;
-		waterInSquare(matrix, n, X - 1, Y);
-	}
+	matrix[X][Y] = 0;
+	return max(max(waterInSquare(matrix, n, X + 1, Y, counter + 1), waterInSquare(matrix, n, X, Y - 1, counter + 1)),
+		max(waterInSquare(matrix, n, X, Y + 1, counter + 1), waterInSquare(matrix, n, X - 1, Y, counter + 1)));
 }
 
 int main()
@@ -97,9 +37,26 @@ int main()
 			cin >> matrix[i][j];
 		}
 	}
-
-	waterInSquare(matrix, n, X, Y);
-	cout << counter << endl;
+	unsigned int time = waterInSquare(matrix, n, X, Y);
+	bool isFine = true;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (matrix[i][j] != 0)
+			{
+				isFine = false;
+			}
+		}
+	}
+	if (isFine)
+	{
+		cout << time << endl;
+	}
+	else
+	{
+		cout << "-1" << endl;
+	}
 
 	return 0;
 }
