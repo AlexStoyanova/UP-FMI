bc5b59d7fc676cf03ca85d1e6052a06c65a1b0ce
AlexStoyanova <al.stoqnova74@gmail.com>
2019-01-13 19:35:14 +0200

    Add second task for second homework for practicum

---
+++ Homework_2_Pract_Task_2/Homework_2_Pract_Task_2/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 100;
+
+bool isEven(int number)
+{
+	return number % 2 == 0;
+}
+
+bool isInNum(int number, int k)
+{
+	while (number)
+	{
+		if (number % 100 == k)
+		{
+			return true;
+		}
+		number /= 10;
+	}
+	return false;
+}
+
+int* filter(int *arr, size_t size, bool(*ptrEven)(int), bool(*ptrIsIn)(int, int), size_t &len)
+{
+	unsigned int count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (ptrIsIn(arr[i], 81))
+		{
+			count++;
+		}
+	}
+	len = size - count;
+	int *newArr = new(nothrow) int[len];
+	if (newArr != nullptr)
+	{
+		unsigned int j = 0;
+		for (int i = 0; i < size; i++)
+		{
+			if (!ptrIsIn(arr[i], 81))
+			{
+				newArr[j] = arr[i];
+				j++;
+			}
+		}
+	}
+	return newArr;
+}
+
+int main()
+{
+	unsigned int n;
+	unsigned int boys;
+	unsigned int girls;
+	int array[SIZE];
+	cin >> n >> boys >> girls;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> array[i];
+	}
+	unsigned int len = 0;
+	int *ptr = filter(array, n, isEven, isInNum, len);
+	unsigned int giftsForGirls = 0;
+	unsigned int giftsForBoys = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (isEven(ptr[i]))
+		{
+			giftsForGirls++;
+		}
+		else
+		{
+			giftsForBoys++;
+		}
+	}
+	cout << "Gifts for boys: " << giftsForBoys << endl << "Gifts for girls: " << giftsForGirls << endl;
+	delete[] ptr;
+
+
+	return 0;
+}
